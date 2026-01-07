7952e21e184a775e665373d08fdaf4dd53facb31
fabiohenrique86 <fabiohenrique86@gmail.com>
2026-01-07 16:07:06 -0300

    creating health check controller

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Azure.Template.Controllers
+{
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                status = "healthy",
+                version = "1.0"
+            });
+        }
+    }
+}
